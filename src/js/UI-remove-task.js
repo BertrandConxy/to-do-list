855f04e94371e738cb12fd1e04e8b365c41f417f
Bertrand Mutangana Ishimwe <mutanganabertrand@gmail.com>
2022-04-30 15:40:23 +0200

    removed unnecessary commented codes

--- src/js/UI-remove-task.js
+++ src/js/UI-remove-task.js
@@ -1,6 +1,5 @@
 export default class RemoveActivities {
   static removeAct(target) {
-    // target.parentNode.parentNode.RemoveActivities(target.parentNode)
     target.parentNode.parentNode.removeChild(target.parentNode);
   }
 
